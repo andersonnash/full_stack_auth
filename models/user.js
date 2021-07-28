06aedbd9ac98acdff2cf37183bf6e7786177df20
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-07-28 12:52:29 -0600

    more functions

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true },
     password_digest: { type: String, required: true, select: false },
   },
