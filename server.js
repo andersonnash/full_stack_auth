016a4e6d1c9a54de03f569d4c5296faff8d40ac2
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-07-28 09:05:11 -0600

    edited server.js and scripts

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 4567;
+
+//gives access to req.body
+app.use(express.json());
+app.use(cors());
+//change after deployment - tiny
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("<h1>Hello, World!</h1>"));
+
+app.listen(PORT, console.log(`conected to port: ${PORT}`));
